28646ba36d70fe4285b806dceba02136d78c044f
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2016-07-26 23:10:43 +0200

    Moved FLAGS to __init__.py

--- tf_lipnet/tf_lipnet_train.py
+++ tf_lipnet/tf_lipnet_train.py
@@ -5,15 +5,7 @@ from datetime import datetime
 import contextlib
 import numpy as np
 import os.path
-
-FLAGS = tf.app.flags.FLAGS
-
-# Directory for train information output. It will be emptied at the beginning of every run
-tf.app.flags.DEFINE_string('log_dir', './output/log', """Directory where to write event logs.""")
-tf.app.flags.DEFINE_string('checkpoint_dir', './output/checkpoint', """Directory where to write checkpoint""")
-tf.app.flags.DEFINE_integer('max_steps', 1000000, """Maximum number of training epochs""")
-tf.app.flags.DEFINE_boolean('log_device_placement', False, """Whether to log device placement""")
-
+from . import FLAGS
 
 @contextlib.contextmanager
 def printoptions(*args, **kwargs):
@@ -42,24 +34,21 @@ def train(particles_df, path_to_images, max_steps):
     :return:
     """
     # Prepare output directories. Empty them if exist, otherwise create
-    _prepare_dir(FLAGS.log_dir)
+    _prepare_dir(FLAGS.log_train_dir)
     _prepare_dir(FLAGS.checkpoint_dir)
 
     with tf.Graph().as_default():
         global_step = tf.Variable(0, trainable=False)
 
-        # batch size. pass it as parameter later
-        batch_size = 500
-
         # get images and labels, training set
         images, labels = tf_lipnet_input.inputs(particles_df,
                                                 path_to_images,
-                                                batch_size=batch_size)
+                                                batch_size=FLAGS.batch_size)
         images = tf.cast(images, tf.float32)
         labels = tf.cast(labels, tf.float32)
         # Build a graph that computes the logits predictions.
         # predictions - predicted probabilities of belonging to any of classes
-        logits, predictions = tf_lipnet.get_predictions(images, batch_size=batch_size)
+        logits, predictions = tf_lipnet.get_predictions(images, batch_size=FLAGS.batch_size)
 
         # calculate loss
         loss = tf_lipnet.get_loss(logits, labels)
@@ -69,7 +58,7 @@ def train(particles_df, path_to_images, max_steps):
 
         # Build a Graph that trains the model with one batch of examples
         # and updates the model parameters
-        train_op = tf_lipnet.train(loss, global_step, batch_size)
+        train_op = tf_lipnet.train(loss, global_step, FLAGS.batch_size)
 
         # Create a saver
         saver = tf.train.Saver(tf.all_variables())
@@ -88,15 +77,15 @@ def train(particles_df, path_to_images, max_steps):
         tf.train.start_queue_runners(sess=sess)
 
         # Create a summary writer
-        summary_writer = tf.train.SummaryWriter(FLAGS.log_dir, sess.graph)
+        summary_writer = tf.train.SummaryWriter(FLAGS.log_train_dir, sess.graph)
         max_steps = 201
+        format_str = '%s: step %d, loss = %.4f, accuracy = %.4f'
         for step in range(max_steps):
             _, loss_value, acc = sess.run([train_op, loss, accuracy])
+            print format_str % (datetime.now(), step, loss_value, acc)
             if step % 10 == 0:
                 summary_str = sess.run(summary_op)
                 summary_writer.add_summary(summary_str, step)
-                format_str = '%s: step %d, loss = %.4f, accuracy = %.4f'
-                print format_str % (datetime.now(), step, loss_value, acc)
                 #with printoptions(precision=4, suppress=True):
                 #    print p
                 # save model to checkpoint
